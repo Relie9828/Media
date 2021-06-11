1ba66925e4b7ee41d24f597eef9934c1490215d2
Aurelien Essome <aubessome98@gmail.com>
2021-06-11 08:25:54 -0700

    06/11/2021

--- functions/index.js
+++ functions/index.js
@@ -10,16 +10,41 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello, Aurelien");
 });
 
+// Getting or calling screams already in the database
+
 exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore()
-        .collection('screams')
+    admin
+        .firestore()
+        .collection('scream')
         .get()
-        .then(data => {
-            let screams =[];
+        .then((data) => {
+            let scream = [];
             data.forEach((doc) => {
-                screams.push(doc.data())
-            })
-            return res.json(screams);
+                scream.push(doc.data());
+            });
+            return res.json(scream);
         })
-        .catch((err) => console.error.error(err))
-})
+        .catch((err) => console.error.error(err));
+});
+
+// This script would be to add a new scream to the database
+
+exports.createScream = functions.https.onRequest((req, res) => {
+   const newScream = {
+       body: req.body.body,
+       userHandle: req.body.userHandle,
+       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+   };
+   
+   admin
+    .firestore()
+    .collection('scream')
+    .add(newScreams)
+    .then((doc) => {
+        req.json({ message: `document ${doc.id} created succesfully`});
+    })
+    .catch((err) => {
+        res.status(500).json({error: 'something went wrong'});
+        console.error(err);
+    });
+});
