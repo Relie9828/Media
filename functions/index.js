56833f01ef8d7a6e42b569d5c77de03a8356346a
Aurelien Essome <aubessome98@gmail.com>
2021-06-18 17:45:32 -0400

    06/18/2021

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+//when building the data base for ... I'll change the 'scream' to 'massage' or 'post'
+// but use the same user(Auth)
+
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const app = require('express')();
@@ -15,14 +18,14 @@ const firebaseConfig = {
   };
 
 //config has been passed here so firebase knows which one we're talking about
-const firebase = reqire('firebase');
+const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
-
+//where ever we see firestore will be replced with 'db'
+const db = admin.firestore();
 
 app.get ('/scream', (req, res) => {
-    admin
-    .firestore()
+   db
     .collection('scream')
     // this orders the screams by date
     .orderBy('createdAt', 'desc')
@@ -76,8 +79,7 @@ app.post('/scream', (req, res) => {
        createdAt: new Date().toISOString()
    };
    
-   admin
-    .firestore()
+  db
     .collection('scream')
     .add(newScream)
     .then((doc) => {
@@ -89,6 +91,18 @@ app.post('/scream', (req, res) => {
     });
 });
 
+//checks for characters matching
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(emailRegEx)) return true;
+    else return false;
+}
+
+//this will prevent white spaces
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
 
 //sign up Route
 app.post('/signup', (req, res) => {
@@ -96,22 +110,96 @@ app.post('/signup', (req, res) => {
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
+       handle: req.body.handle,
     }
 
-    //TODO validate data
-firebase
-    .auth()
-    .createUserWithEmailAndPAssword(newUser.email, newUser.password)
-    .then(data =>{
-        return res.status(201).json({message:`user ${data.user.uid} You're now signed up`});
+let errors = {};
+
+
+//check email validation & that it's not empty
+if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty'
+} else if(!isEmail(newUser.email)){
+    errors.email = 'Must be a valid email address'
+}
+
+//checking for password matching & not empty
+if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+//TODO validate data
+let token, userId;
+db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+        if(doc.exists){
+            return res.status(400).json({handle: 'This is already taken'})
+        }else {
+           return firebase
+                .auth()
+                .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        }
     })
+    .then((data) => {
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+           handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date() .toISOString(),
+            userId
+        }
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() =>{
+        return res.status(201).json({token});
+    })
+
+
     .catch((err) => {
         console.error((err));
-        return res.status(500).json({erroe: err.code});
-    });
+        if(err.code === 'auth/email-already-in-use'){
+            return res.status(400).json({email: 'Email already in use'})
+        }else {
+            return res.status(500).json({error: err.code});
+        }
+    })
+
 })
 
+//login confirmation & auth
+app.post('/login', (req,res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
 
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = "Must not be empty";
+    if(isEmpty(user.password)) errors.password = "Must not be empty";
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+        return data.user.getIdToken()
+    })
+    .then(token => {
+        return res.json({token});
+    })
+    .catch(err =>{
+        console.error(err);
+        if(err.code === 'auth/wrong-password'){
+            return res.status(403).json({general: 'Wrong password or email'})
+        }else return res.status(500).json({error: err.code})
+    });
+})
 // the ./app now needs to be exported so i can be exported as an API
 exports.api = functions.https.onRequest(app);
